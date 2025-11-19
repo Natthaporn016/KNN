3e7e994903415cb1b1ebf7a4da26527d71a6f396
Natthaporn016 <664230016@webmail.npru.ac.th>
2025-11-19 15:33:42 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,10 +7,12 @@ import matplotlib.pyplot as plt
 
 st.header("Pin")
 st.image("./img/pin.jpg")
+col1,col2,col3 = st.columns(3)
 
 with col1:
     st.header("Versicolor")
     st.image("./img/iris1.jpg")
+
 
 with col2:
     st.header("Verginiga")
